42e8b3a186d106645fa6ead484c3287d4e17bc30
Rahulll066 <rahulsainadhmail@gmail.com>
2025-12-24 18:20:23 +0530

    Fixing cors error 3

--- orbit-backend/src/index.ts
+++ orbit-backend/src/index.ts
@@ -13,15 +13,32 @@ import cors from "cors";
 await connectToDB();
 
 const app = express();
-app.use(cors({
-  origin: [
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
     CORS_ORIGIN
-  ],
-}));
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Orbit Backend is running 🚀");
+  res.send("Orbit Backend is running!...");
 });
 
 app.get("/health", (req, res) => {
